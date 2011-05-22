02929917823a18a36a99d974b938ca4809d6151e
Mikhail Glushenkov <the.dead.shall.rise@gmail.com>
2011-05-22 12:25:15 +0200

    Small update.

--- server/src/Main.hs
+++ server/src/Main.hs
@@ -10,7 +10,10 @@ import Snap.Types
 site :: Snap ()
 site = route [ ("/about", serveFile "static/about.html")
              , ("/code",  serveFile "static/code.html")
-             ] <|> serveDirectory "static"
+             ]
+       <|> serveDirectory "static"
+       <|> serveFile "static/404.html"
+
 
 main :: IO ()
 main = quickHttpServe site
